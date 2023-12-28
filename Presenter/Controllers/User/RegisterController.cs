b7608fcc51ba67787a367d5a9e9f66c8d91b6e07
Muflih majid <bsi80113@bsi.co.id>
2023-12-28 11:14:22 +0700

    Update router for api

--- Presenter/Controllers/User/RegisterController.cs
+++ Presenter/Controllers/User/RegisterController.cs
@@ -1,18 +1,27 @@
-using System.IO;
+﻿using System.IO;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using SceletonAPI.Application.UseCases.RegisterUser;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace SceletonAPI.Presenter.Controllers.Register {
     [ApiController]
     [Produces (MediaTypeNames.Application.Json)]
-    public class LoginController : BaseController {
+    [Route("/user")]
+    public class RegisterController : BaseController {
         [HttpPost]
-        [Route ("/login/user")]
+        [Route ("/user/register")]
         [ProducesResponseType (StatusCodes.Status200OK)]
         public async Task<ActionResult<RegisterUserDto>> Register ([FromBody] RegisterUserCommand Payload) {
+            return Ok (await Mediator.Send (Payload));
+        }
+
+        [HttpPost]
+        [Route ("/user/login")]
+        [ProducesResponseType (StatusCodes.Status200OK)]
+        public async Task<ActionResult<RegisterUserDto>> login ([FromBody] RegisterUserCommand Payload) {
             return Ok (await Mediator.Send (Payload));
         }
     }
